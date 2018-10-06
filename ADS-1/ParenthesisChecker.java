11bb441768d9645092c7558fc359011c07d0c6f7
tbharathsai20 <tbharathsai20@gmail.com>
2018-10-06 11:32:29 +0530

    module-4

---
+++ ADS-1/ParenthesisChecker.java
@@ -0,0 +1,117 @@
+ import java.util.Scanner;
+
+class Stack
+{
+	int N=0;
+	char [] s;
+	public Stack(int cap)
+	{
+		s=new char[cap];
+	}
+	public boolean isEmpty()
+	{
+		return N==0;
+	}
+	public void push(char a)
+	{
+		if(!isFull())
+		s[N++]=a;
+	}
+	public char pop()
+	{
+		char b = 0;
+		if(!isEmpty())
+		{
+		 b=s[--N];
+		return b;
+		}
+		return b;
+	}
+	public boolean isFull()
+	{
+		return N==s.length;
+	}
+	public int peek()
+	{
+		return s[N];
+	}
+	public void Empty()
+	{
+		N=0;
+	}
+}
+class ParenthesisChecker
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String N=sc.nextLine();
+		//Stack st=new Stack(N);
+		char r;
+		int count=0;
+		boolean t=true;
+		while(sc.hasNext())
+		{
+			String s=sc.nextLine();
+			Stack st=new Stack(s.length());
+			for(int i=0;i<s.length();i++)
+			{
+				char c=s.charAt(i);
+				switch(c)
+				{
+				case '{':
+					st.push(c);
+					break;
+				case '[':
+					st.push(c);
+					break;
+				case '(':
+					st.push(c);
+					break;
+				case '}':
+					r=st.pop();
+					t=(r=='{');
+					//System.out.println("case 1  :"+t);
+					break;
+				case ']':
+					r=st.pop();
+					t=(r=='[');
+					//System.out.println("case 2  :"+t);
+					break;
+				case ')':
+					r=st.pop();
+					t=(r=='(');
+					//System.out.println("case 3 :"+t);
+					break;
+				}
+				if(t==false)
+				{
+					break;
+				}
+				
+			}
+			if(t==false)
+			{
+				t=true;
+				System.out.println("NO");
+				count++;
+			}
+			else
+			{
+				System.out.println("YES");
+				count++;
+			}
+			//System.out.println(count);
+			st.Empty();
+		}
+		/*if(t==true)
+		{
+			System.out.print(true);
+		}
+		else
+		{
+			System.out.print(false);
+		}*/
+		//System.out.println(count);
+	}
+}
---
+++ ADS-1/Solution .java
@@ -0,0 +1,61 @@
+package com.xyz;
+import java.util.Scanner;
+import java.util.Stack; 
+
+public class Solution  
+{ 
+    // Method to evaluate value of a postfix expression 
+    static int evaluatePostfix(String exp) 
+    { 
+        //create a stack 
+        Stack<Integer> stack=new Stack<Integer>(); 
+          
+        // Scan all characters one by one 
+        for(int i=0;i<exp.length();i++) 
+        { 
+            char c=exp.charAt(i); 
+              
+            // If the scanned character is an operand (number here), 
+            // push it to the stack. 
+            if(Character.isDigit(c)) 
+            stack.push(c - '0'); 
+              
+            //  If the scanned character is an operator, pop two 
+            // elements from stack apply the operator 
+            else
+            { 
+                int val1 = stack.pop(); 
+                int val2 = stack.pop(); 
+                  
+                switch(c) 
+                { 
+                    case '+': 
+                    stack.push(val2+val1); 
+                    break; 
+                      
+                    case '-': 
+                    stack.push(val2- val1); 
+                    break; 
+                      
+                    case '/': 
+                    stack.push(val2/val1); 
+                    break; 
+                      
+                    case '*': 
+                    stack.push(val2*val1); 
+                    break; 
+              } 
+            } 
+        } 
+        return stack.pop();     
+    } 
+      
+    // Driver program to test above functions 
+    public static void main(String[] args)  
+    { 
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("Enter the string");
+        String exp=sc.next(); 
+        System.out.println(evaluatePostfix(exp)); 
+    } 
+} 
