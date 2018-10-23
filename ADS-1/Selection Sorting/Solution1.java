27e377738a6bb14ee27afe7b809299f8cd0143f0
tbharathsai20 <tbharathsai20@gmail.com>
2018-10-23 15:22:33 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS-1/Selection Sorting/Solution1.java
+++ ADS-1/Selection Sorting/Solution1.java
@@ -1,4 +1,55 @@
 
-public class Solution1 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
+class Teams implements Comparable<Teams> {
+	String Tname;
+	int wins;
+	int losses;
+	int draws;
+
+	Teams(String Tname, int wins, int losses, int draws) {
+		this.Tname = Tname;
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+	}
+
+	@Override
+	public int compareTo(Teams Ts) {
+		if (this.wins == Ts.wins) {
+			if (this.losses == Ts.losses) {
+				return Ts.draws - this.draws;
+			} else {
+				return this.losses - Ts.losses;
+			}
+		} else {
+			return Ts.wins - this.wins;
+		}
+
+	}
+}
+
+public class Solution1 {
+	public static void main(String args[]) {
+		ArrayList<Teams> al = new ArrayList<>();
+		Scanner scn = new Scanner(System.in);
+		do {
+			String st = scn.nextLine();
+			String sp[] = st.split(",");
+			al.add(new Teams(sp[0], Integer.parseInt(sp[1]), Integer.parseInt(sp[2]), Integer.parseInt(sp[3])));
+		} while (scn.hasNext());
+		Collections.sort(al);
+		// System.out.println(al);
+		String st = "" + al.get(0).Tname;
+		for (int i = 1; i < al.size(); i++) {
+			st = st + ","+ al.get(i).Tname;
+			
+		}
+		System.out.print(st);
+		// System.out.print(ts.Tname+",");
+	}
 }
